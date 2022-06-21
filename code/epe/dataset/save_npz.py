1a458a005d8dc8ed6cdb1fc43d62b3ee01a7064b
LvZut <33284924+LvZut@users.noreply.github.com>
2022-06-21 10:46:40 +0200

    multithreaded save_npz.py

--- code/epe/dataset/save_npz.py
+++ code/epe/dataset/save_npz.py
@@ -1,3 +1,4 @@
+from readline import get_line_buffer
 import numpy as np
 import imageio.v2 as imageio
 from PIL import Image
@@ -8,13 +9,44 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-# # https://stackoverflow.com/questions/39382412/crop-center-portion-of-a-numpy-image
-# def crop_center(img,cropx,cropy):
-#     y,x = img.shape[:2]
-#     startx = x//2-(cropx//2)
-#     starty = y//2-(cropy//2)    
-#     return img[starty:starty+cropy,startx:startx+cropx,:]
 
+from threading import Thread
+
+def save_data(arguments):
+
+    step_path = arguments['step_path']
+    suffix = arguments['suffix']
+    gbuffer_prefixes = arguments['gbuffer_prefixes']
+    gb_path, rgb_path, gt_path = arguments['save_paths']
+    oneD = arguments['oneD']
+
+    # resize image?
+    img = imageio.imread(f'{step_path}rgb_{suffix}.jpeg', pilmode='RGB')
+    gt_labels = imageio.imread(f'{step_path}sem_seg_{suffix}.png', pilmode='RGB')
+
+    gb_array = np.zeros((720, 1280, 31))
+    arr_index = 0
+
+    for prefix in gbuffer_prefixes:
+
+        try:
+            gb = imageio.imread(f'{step_path}{prefix}_{suffix}.png', pilmode='RGB')
+        except:
+            print(f'gbuffer {step_path}{prefix}_{suffix} not found for image!')
+            continue
+
+
+        # only use first dimension if grayscale
+        if prefix in oneD:
+            gb_array[:,:,arr_index] = gb[:,:,0]
+            arr_index += 1
+        else:
+            gb_array[:,:,arr_index:arr_index+3] = gb
+            arr_index += 3
+
+    np.savez_compressed(gb_path, img=img, gbuffers=gb_array, shader=gt_labels)
+    imageio.imsave(rgb_path, img)
+    imageio.imsave(gt_path, gt_labels)
 
 def main():
     path = "Town01"
@@ -51,41 +83,20 @@ def main():
                 for suffix in suffixes:
                     step_path = f'{step}/'
 
-                    # resize image?
-                    img = imageio.imread(f'{step_path}rgb_{suffix}.jpeg', pilmode='RGB')
-                    gt_labels = imageio.imread(f'{step_path}sem_seg_{suffix}.png', pilmode='RGB')
-
-                    gb_array = np.zeros((720, 1280, 31))
-                    arr_index = 0
-
-                    for prefix in gbuffer_prefixes:
-
-                        try:
-                            gb = imageio.imread(f'{step_path}{prefix}_{suffix}.png', pilmode='RGB')
-                        except:
-                            print(f'gbuffer {step_path}{prefix}_{suffix} not found for image!')
-                            continue
-
-
-                        # only use first dimension if grayscale
-                        if prefix in oneD:
-                            gb_array[:,:,arr_index] = gb[:,:,0]
-                            arr_index += 1
-                        else:
-                            gb_array[:,:,arr_index:arr_index+3] = gb
-                            arr_index += 3
-
                     # make sure data/carla/gbuffers exists
                     gb_path = f'{carla_path}gbuffers/data_{path}_{index_ep}_{index_step}_{suffix}.npz'
                     rgb_path = f'{carla_path}rgb/rgb_{path}_{index_ep}_{index_step}_{suffix}.png'
                     gt_path = f'{carla_path}semantic/semantic_{path}_{index_ep}_{index_step}_{suffix}.png'
 
+                    arguments = {'step_path' : step_path,
+                                'suffix' : suffix,
+                                'gbuffer_prefixes' : gbuffer_prefixes,
+                                'save_paths' : (gb_path, rgb_path, gt_path),
+                                'oneD' : oneD}
 
+                    Thread(target=save_data, args=(arguments,)).start()
 
-                    np.savez_compressed(gb_path, img=img, gbuffers=gb_array, shader=gt_labels)
-                    imageio.imsave(rgb_path, img)
-                    imageio.imsave(gt_path, gt_labels)
-
+                    # don't write async as that might cause trouble
                     writer.writerow([rgb_path[6:], gt_path[6:], f'{carla_path[6:]}robust_semantic/gray/robust_{path}_{index_ep}_{index_step}_{suffix}.png', gb_path[6:]])
 
 
