1b24c3d067e212b74db0bb9f2cd579e65ade2664
LvZut <louisvincentvanzutphen@gmail.com>
2022-06-27 21:40:50 +0200

    savez

--- code/epe/dataset/save_npz.py
+++ code/epe/dataset/save_npz.py
@@ -11,6 +11,22 @@ from os.path import isfile, join
 
 
 from multiprocessing import Pool
+import logging
+
+import re
+
+start_episode = 0
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 def save_data(arguments):
 
@@ -21,8 +37,12 @@ def save_data(arguments):
     oneD = arguments['oneD']
 
     # resize image?
-    img = imageio.imread(f'{step_path}rgb_{suffix}.jpeg', pilmode='RGB')
-    gt_labels = imageio.imread(f'{step_path}sem_seg_{suffix}.png', pilmode='RGB')
+    try:
+        img = imageio.imread(f'{step_path}rgb_{suffix}.jpeg', pilmode='RGB')
+        gt_labels = imageio.imread(f'{step_path}sem_seg_{suffix}.png', pilmode='RGB')
+    except:
+        logging.info(f'img/gt_labels not found for {step_path}')
+        return 0
 
     gb_array = np.zeros((720, 1280, 31))
     arr_index = 0
@@ -33,7 +53,8 @@ def save_data(arguments):
             gb = imageio.imread(f'{step_path}{prefix}_{suffix}.png', pilmode='RGB')
         except:
             print(f'gbuffer {step_path}{prefix}_{suffix} not found for image!')
-            continue
+            logging.info(f'gbuffer {step_path}{prefix}_{suffix} not found, skipping step')
+            return 0
 
 
         # only use first dimension if grayscale
@@ -43,15 +64,16 @@ def save_data(arguments):
         else:
             gb_array[:,:,arr_index:arr_index+3] = gb
             arr_index += 3
-
+    logging.info(f'Saving {step_path}')
     np.savez_compressed(gb_path, img=img, gbuffers=gb_array, shader=gt_labels)
     imageio.imsave(rgb_path, img)
     imageio.imsave(gt_path, gt_labels)
 
 def main():
-    path = "Town01"
+    path = "Town02"
     gpath = "../../../../data/gbuffers/" + path + '/'
     carla_path = "../../../../data/carla/"
+    save_path = "/hdd2/LvZut/saivvy/data/carla/"
 
     # removed the first 2 temporarily as they are white images
     gbuffer_prefixes = [#'ambientocclusion', 'separatetranslucency',
@@ -65,15 +87,21 @@ def main():
 
     suffixes = ['0_degrees', '90_degrees', '180_degrees', '270_degrees']
 
-    episode_folders = [ f.path for f in os.scandir(gpath) if f.is_dir() ]
+    episode_folders = sorted([ f.path for f in os.scandir(gpath) if f.is_dir() ], key=natural_keys)
     print(f'episodes: {len(episode_folders)}')
+   
+    logging.basicConfig(filename="/hdd2/LvZut/saivvy/data/carla/process_log.log", level=logging.INFO)
+    episode_num = 0
 
-# open the file in the write mode
-    with open(f'{gpath}{path}_files.csv', 'w') as f:
+    with open(f'../../CARLA_files.csv', 'a') as f:
         # create the csv writer
         writer = csv.writer(f)
-        pool = Pool()
-        for index_ep, episode in tqdm(enumerate(episode_folders)):
+        pool = Pool(8)
+
+        if start_episode != 0:
+            episode_folders = episode_folders[start_episode:]
+
+        for index_ep, episode in tqdm(enumerate(episode_folders, start=start_episode)):
             episode_steps = [ f.path for f in os.scandir(episode) if f.is_dir() ]
             # breakpoint()
             arguments_list = []
@@ -85,9 +113,9 @@ def main():
                     step_path = f'{step}/'
 
                     # make sure data/carla/gbuffers exists
-                    gb_path = f'{carla_path}gbuffers/data_{path}_{index_ep}_{index_step}_{suffix}.npz'
-                    rgb_path = f'{carla_path}rgb/rgb_{path}_{index_ep}_{index_step}_{suffix}.png'
-                    gt_path = f'{carla_path}semantic/semantic_{path}_{index_ep}_{index_step}_{suffix}.png'
+                    gb_path = f'{save_path}gbuffers/data_{path}_{index_ep}_{index_step}_{suffix}.npz'
+                    rgb_path = f'{save_path}rgb/rgb_{path}_{index_ep}_{index_step}_{suffix}.png'
+                    gt_path = f'{save_path}semantic/semantic_{path}_{index_ep}_{index_step}_{suffix}.png'
 
                     arguments = {'step_path' : step_path,
                                 'suffix' : suffix,
@@ -98,9 +126,17 @@ def main():
                     arguments_list.append(arguments)
 
                     # don't write async as that might cause trouble
-                    writer.writerow([rgb_path[6:], gt_path[6:], f'{carla_path[6:]}robust_semantic/gray/robust_{path}_{index_ep}_{index_step}_{suffix}.png', gb_path[6:]])
-
+                    writer.writerow([rgb_path, gt_path, f'{save_path}robust_semantic/gray/robust_{path}_{index_ep}_{index_step}_{suffix}.png', gb_path])
+            
             pool.map(save_data, arguments_list)
+            episode_num += 1
+
+            if episode_num % 100 == 0:
+                try:
+                    logging.info(f'processed {episode_num/len(episode_folders)} ({episode_num}/{len(episode_folders)})')
+                except:
+                    pass
+    pool.close()
     pool.join()
 
 if __name__ == "__main__":
