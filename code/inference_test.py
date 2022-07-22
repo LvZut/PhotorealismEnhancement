2ce74d8dcb177091b9e0a02de4f2c4ef8d83394f
LvZut <33284924+LvZut@users.noreply.github.com>
2022-07-22 16:18:09 +0200

    Update inference_test.py

--- code/inference_test.py
+++ code/inference_test.py
@@ -14,9 +14,8 @@ task = InferenceTask(robust_cfg, 0, 0, 0, '', 'universal', 'universal', robust_c
 img1 = cv2.imread('../../saivvy/data/carla/rgb/rgb_Town01_1000_3_90_degrees.png', cv2.IMREAD_COLOR)
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 img1 = np.float32(img1)
-img1 = img1[:480, :720,:]
 
-task.base_size=min(img1.shape[0], img1.shape[1])
+task.base_size=1080
 task.crop_h=img1.shape[0]
 task.crop_w=img1.shape[1]
 
