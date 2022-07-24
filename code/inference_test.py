fa69a39afe10cf34433cd3e8b78030a881316792
LvZut <33284924+LvZut@users.noreply.github.com>
2022-07-24 10:15:54 +0200

    Update inference_test.py

--- code/inference_test.py
+++ code/inference_test.py
@@ -9,15 +9,15 @@ robust_cfg = config.load_cfg_from_cfg_file('config/robust_config/config_1080.yam
 assert isinstance(robust_cfg.model_name, str)
 assert isinstance(robust_cfg.model_path, str)
 
-task = InferenceTask(robust_cfg, None, 0, 0, '', 'universal', 'universal', robust_cfg.scales)
+task = InferenceTask(robust_cfg, None, 713, 713, '', 'universal', 'universal', robust_cfg.scales)
 
 img1 = cv2.imread('../../saivvy/data/carla/rgb/rgb_Town01_1000_3_90_degrees.png', cv2.IMREAD_COLOR)
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 img1 = np.float32(img1)
 
 # task.base_size=1280
-task.crop_h=img1.shape[0]
-task.crop_w=img1.shape[1]
+# task.crop_h=img1.shape[0]
+# task.crop_w=img1.shape[1]
 
 
 out = task.execute_on_img(image=img1)
