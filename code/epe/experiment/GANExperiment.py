71c1a3951d626637918aefb561c33692db196143
LvZut <33284924+LvZut@users.noreply.github.com>
2022-08-17 14:38:00 +0200

    Update GANExperiment.py

--- code/epe/experiment/GANExperiment.py
+++ code/epe/experiment/GANExperiment.py
@@ -217,7 +217,7 @@ class GANExperiment(BaseExperiment):
 
                 with torch.no_grad():
                         for bi, batch_fake in enumerate(self.loader_fake):                
-                                print(f'batch {bi}, size {batch_fake.size()}')
+                                print(f'batch {bi}, size {batch_fake.img.size()}')
                                 self.save_result(self.evaluate_test(batch_fake.to(self.device), bi), bi)
                                 pass
                         pass
