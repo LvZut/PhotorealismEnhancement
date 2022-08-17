d2a4173b16e9c831e7ca143c702045fc1da053dd
LvZut <33284924+LvZut@users.noreply.github.com>
2022-08-17 15:12:56 +0200

    Update show_imgs.py

--- code/show_imgs.py
+++ code/show_imgs.py
@@ -18,28 +18,33 @@ steps = list(range(1001, 1099, 2))
 
 
 for step in steps:
-    rec = torch.load(f'gen_out/rec_{step}.pt', map_location=torch.device('cpu'))
-    robust = torch.load(f'gen_out/robust_{step}.pt', map_location=torch.device('cpu'))
-    inp = torch.load(f'gen_out/input_{step}.pt', map_location=torch.device('cpu')).detach().numpy()
-    outp = torch.load(f'gen_out/output_{step}.pt', map_location=torch.device('cpu')).detach().numpy()
+    rec_robust = torch.load(f'gen_out/rec_{step}.pt', map_location=torch.device('cpu'))
+    inp_labels = torch.load(f'gen_out/robust_{step}.pt', map_location=torch.device('cpu'))
+    img = torch.load(f'gen_out/input_{step}.pt', map_location=torch.device('cpu')).detach().numpy()
+    rec = torch.load(f'gen_out/output_{step}.pt', map_location=torch.device('cpu')).detach().numpy()
 
     inp_robust = torch.from_numpy(mseg_inference.inference(inp.copy())[0])
 
     # input
     plt.subplot(2, 3, 1)
-    plt.imshow(  np.transpose(inp[0], (1,2,0))  )
+    plt.imshow(  np.transpose(img[0], (1,2,0))  )
+    plt.suptitle('input')
     # output
     plt.subplot(2, 3, 2)
-    plt.imshow(  np.transpose(outp[0], (1,2,0))  )
+    plt.imshow(  np.transpose(rec[0], (1,2,0))  )
+    plt.suptitle('output')
     # rec
     plt.subplot(2, 3, 3)
-    plt.imshow(  rec  )
+    plt.imshow(  rec_robust  )
+    plt.suptitle('rec')
     # inp_robust
     plt.subplot(2, 3, 4)
     plt.imshow(  inp_robust  )
     plt.savefig(f'gen_out/results_{step}.png')
+    plt.suptitle('inp robust')
     # outp_robust
     plt.subplot(2, 3, 5)
-    plt.imshow(  robust[0,0,:,:]  )
+    plt.imshow(  inp_labels[0,0,:,:]  )
+    plt.suptitle('out robust')
     
     print(step)
