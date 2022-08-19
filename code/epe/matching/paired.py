7ca795a14c739b6053735f0b4f75af079eea1e40
LvZut <33284924+LvZut@users.noreply.github.com>
2022-08-19 14:13:01 +0200

    Update paired.py

--- code/epe/matching/paired.py
+++ code/epe/matching/paired.py
@@ -147,16 +147,23 @@ class FullImages(PairedDataset):
 	# 	def __getitem__(self, idx):
 	# 		return JointEPEBatch(self._source_dataset[idx], self._target_dataset[random.randint(0, len(self._target_dataset)-1)])
 
-	def _sample_crop(self, batch):
+	def _sample_full(self, batch):
 		r1 = batch.img.shape[-2]
 		r0 = 0
 		c1 = batch.img.shape[-1]
 		c0 = 0
 		return batch.crop(r0, r1, c0, c1)
 
+	def _sample_crop(self, batch, shape0, shape1):
+		r1 = random.randint(shape0, batch.img.shape[-2])
+		r0 = r1 - shape0
+		c1 = random.randint(shape1, batch.img.shape[-1])
+		c0 = c1 - shape1
+		return batch.crop(r0, r1, c0, c1)
+
 
 	def __getitem__(self, idx):
-		return JointEPEBatch(self._sample_crop(self._source_dataset[idx]), \
+		return JointEPEBatch(self._sample_full(self._source_dataset[idx]), \
 			self._sample_crop(self._target_dataset[random.randint(0, len(self._target_dataset)-1)]))
 
 	def __len__(self):
