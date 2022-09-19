3e5edeb14dd9c9fd952c5e02f62033d81317028b
LvZut <33284924+LvZut@users.noreply.github.com>
2022-09-19 12:36:38 +0200

    Update evaluation_dataloader.py

--- code/epe/dataset/evaluation_dataloader.py
+++ code/epe/dataset/evaluation_dataloader.py
@@ -6,7 +6,7 @@ class evaluation_dataloader(Dataset):
     """ Wrapper around dataloader that performs inference
         using generator model given in class init """
 
-    def __init__(self, dataset, gen, collate_fn_val, seed_worker):
+    def __init__(self, dataset, gen, collate_fn_val, seed_worker, transform=None):
         """
         Args:
             dataloader (Torch dataloader) : Feeds images to generator
@@ -14,7 +14,7 @@ class evaluation_dataloader(Dataset):
         """
         self.gen = gen
         self.dataloader = torch.utils.data.DataLoader(self.dataset_fake_val, batch_size=1, shuffle=True, num_workers=0, pin_memory=True, drop_last=False, collate_fn=collate_fn_val, worker_init_fn=seed_worker)
-        
+        self.transform = transform
 
     def __len__(self):
         return len(self.dataloader)
