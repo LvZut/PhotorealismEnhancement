943b8d522b22bf1dcf261e3b613edbc491e0f9af
LvZut <33284924+LvZut@users.noreply.github.com>
2022-09-19 14:34:48 +0200

    Update evaluation_dataloader.py

--- code/epe/dataset/evaluation_dataloader.py
+++ code/epe/dataset/evaluation_dataloader.py
@@ -26,5 +26,5 @@ class evaluation_dataloader(Dataset):
         batch = self.dataset[idx]
         model_out = self.gen(batch.to(self.device))
         del batch
-        # breakpoint()
+        breakpoint()
         return {'images' : model_out}
