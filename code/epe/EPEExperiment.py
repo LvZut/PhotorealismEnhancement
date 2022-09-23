923fb4092fde7b9080448715de79b72b746f2797
LvZut <33284924+LvZut@users.noreply.github.com>
2022-09-23 09:40:31 +0200

    Update EPEExperiment.py

--- code/epe/EPEExperiment.py
+++ code/epe/EPEExperiment.py
@@ -217,10 +217,6 @@ class EPEExperiment(ee.GANExperiment):
                 self._log.info('Initializing networks ...')
 
                 # network arch depends on dataset
-                if self.action == 'evaluate_model':
-                        self.gen_cfg['num_classes']          = self.dataset_train.num_classes
-                        self.gen_cfg['num_gbuffer_channels'] = self.dataset_train.num_gbuffer_channels
-                        self.gen_cfg['cls2gbuf']             = self.dataset_train.cls2gbuf
                 elif self.dataset_train is not None:
                         self.gen_cfg['num_classes']          = self.dataset_train.source.num_classes
                         self.gen_cfg['num_gbuffer_channels'] = self.dataset_train.source.num_gbuffer_channels
